30079161fa4244c720d094b792d763e758a4bf75
Sebastine Odeh <52936414+CoderNamedHendrick@users.noreply.github.com>
2022-04-28 15:00:12 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
     <script src="//code.tidio.co/sddr9cnjk4ydmxtmzheha5kmaj7xhf2h.js" async></script>
       </Head>
+    const initChat = () => {
+      tidioChatApi.open();
+    }
+    
+    (function () {
+    function onTidioChatApiReady() {
+        window.tidioChatApi.open();
+    }
+    if (window.tidioChatApi) {
+        window.tidioChatApi.on('ready', onTidioChatApiReady);
+    } else {
+        document.addEventListener('tidioChat-ready', onTidioChatApiReady);
+    }
+})();
     </div>
   )
 }
