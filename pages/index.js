3085689c2ac2c06c61fa64a3dbaf74573336716b
Sebastine Odeh <52936414+CoderNamedHendrick@users.noreply.github.com>
2022-08-03 18:55:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,22 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-    <script src="//code.tidio.co/sddr9cnjk4ydmxtmzheha5kmaj7xhf2h.js" async></script>
+        <script src="//code.tidio.co/sddr9cnjk4ydmxtmzheha5kmaj7xhf2h.js" async></script>
       </Head>
     <script>
     (function() {
-  function onTidioChatApiReady() {
-    // Code after chat loaded
-    if (window.tidioChatApi) {
-    window.tidioChatApi.on("ready", onTidioChatApiReady);
-  } else {
-    document.addEventListener("tidioChat-ready", onTidioChatApiReady);
-  }We 
-  }
-})();
+      function onTidioChatApiReady() {
+        // Code after chat loaded
+        if (window.tidioChatApi) {
+          window.tidioChatApi.on("ready", onTidioChatApiReady);
+        } else {
+          document.addEventListener("tidioChat-ready", onTidioChatApiReady);
+        }We 
+      }
+  })();
+ 
   
-  function waitForElm(selector) {
+  const waitForElm = (selector) => {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
             return resolve(document.querySelector(selector));
